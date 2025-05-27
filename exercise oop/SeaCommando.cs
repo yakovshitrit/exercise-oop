e047ecd335058315fad3e5babd02332706a887da
yakov shitrit <9090yakov@gmail.com>
2025-05-27 12:24:03 +0300

    inheritance

---
+++ exercise oop/SeaCommando.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_oop
+{
+    internal class SeaCommando: Commando
+    {
+        public SeaCommando(string name , int codename ,string status):base(name, codename,status) { }
+        public void Swim()
+        {
+            Status = "swim";
+        }
+    }
+}
