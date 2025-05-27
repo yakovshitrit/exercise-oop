bd7437a07fe248b689d9829b9ed782472a447e68
yakov shitrit <9090yakov@gmail.com>
2025-05-27 09:37:54 +0300

    Add project files.

---
+++ exercise oop/Commando.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_oop
+{
+    internal class Commando
+    {
+        string Name;
+        int CodeName;
+        string[] Tool;
+        string Status;
+
+        public  Commando(string name,int codename, string[] tool, string status)
+        {
+            this.Name = name;
+            this.CodeName = codename;
+            this.Tool = tool;
+            
+        }
+            
+            
+
+            
+            
+            
+
+
+
+
+
+
+
+
+    }
+}
